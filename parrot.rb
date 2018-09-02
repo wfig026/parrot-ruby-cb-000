814f436129387dc15cd23d85a000bcfd22bbea9f
Will Figueiredo <will@wfigueiredo.com>
2018-09-02 21:18:16 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase)
-  puts "#{phrase}"
+def parrot
+  puts "Squawk!"
 end
 
-phrase("Squawk!")
+parrot
